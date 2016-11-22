d6692aa97f03ab1aafa2a98a42148e0346b885df
Tom Clark <tom.wil.clark@gmail.com>
2016-11-22 15:46:45 +0000

    working for one league match, all goals home and away to CSV

--- get_details.py
+++ get_details.py
@@ -6,8 +6,8 @@ import urllib.request
 
 import csv
 
-def download_details(url):
-    result = requests.get(url)
+def download_details(url,i):
+    result = requests.get(url + str(i))
 
     if result.status_code != 200:
         return None
@@ -15,30 +15,33 @@ def download_details(url):
     content = result.content
 
     soup = BeautifulSoup(content, "html.parser")
-
-    page_results = soup.find_all(class_="search-page__result")
-    #j = 0
-    for page_result in page_results:
-        #j = j + 1
-        search_result_content = page_result.find(class_="search-result standard ").find(class_="search-result__r1").find(class_="search-result__content ")
-
-        car_id = page_result.get("id")
-
-        for li in search_result_content.find(class_="search-result__attributes").findAll("li"):
-            if "miles" in li.text:
-                mileage = li.text.replace(',', '').replace('miles', '').strip()
-
-
-        for li in search_result_content.find(class_="search-result__attributes").findAll("li"):
-            if "reg" in li.text:
-                age = li.text.strip()[:4]
-
-        price = search_result_content.find(class_="search-result__titles").find(class_="search-result__price").text.strip().ljust(6)[1:].replace(',', '')
-        car_info = [car_id, age, price, mileage]# img_page = image_cell.a['href']
-        print(car_info)
-        with open('cars.csv', 'a', newline='') as myfile:
+    home_name = soup.find(class_="teamsContainer").find(class_="home").find(class_="teamName").find(class_="long").text
+    away_name = soup.find(class_="teamsContainer").find(class_="away").find(class_="teamName").find(class_="long").text
+    home_div = soup.find(class_="matchEventsContainer").find(class_="home")
+    away_div = soup.find(class_="matchEventsContainer").find(class_="away")
+    home_events = home_div.find_all(class_="event")
+    away_events = away_div.find_all(class_="event")
+    for event in home_events:
+        trimmed_event = event.text.strip()
+        event_len = len(trimmed_event)
+        player = trimmed_event[:event_len-3].strip()
+        goal_time = int(trimmed_event[event_len-3:][:2])
+        line_info = [i, home_name, away_name, player, home_name, goal_time]
+        print(line_info)
+        with open('results.csv', 'a', newline='') as myfile:
             wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-            wr.writerow(car_info)
+            wr.writerow(line_info)
+
+    for event in away_events:
+        trimmed_event = event.text.strip()
+        event_len = len(trimmed_event)
+        player = trimmed_event[:event_len-3].strip()
+        goal_time = int(trimmed_event[event_len-3:][:2])
+        line_info = [i, home_name, away_name, player, away_name, goal_time]
+        print(line_info)
+        with open('results.csv', 'a', newline='') as myfile:
+            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+            wr.writerow(line_info)
 
         # img_location = image_cell.img['src']
         # med_location = img_location.upper().find("/MED/")
@@ -48,17 +51,15 @@ def download_details(url):
         # print (str(j).rjust(2) + ": " + file_name)
         # urllib.request.urlretrieve(img_location,"downloaded_images/" + file_name)
 #original URL had a limit on age
-#url_front = "http://www.autotrader.co.uk/car-search?sort=distance&radius=1500&postcode=bs84ye&onesearchad=Used&make=MINI&model=HATCH&year-from=2003&maximum-mileage=125000&body-type=Hatchback&fuel-type=Petrol&maximum-badge-engine-size=1.6&transmission=Manual&quantity-of-doors=3&seller-type=private&keywords=cooper&page="
-url_front = "http://www.autotrader.co.uk/car-search?sort=distance&radius=1500&postcode=bs84ye&onesearchad=Used&make=MINI&model=HATCH&maximum-mileage=125000&body-type=Hatchback&fuel-type=Petrol&maximum-badge-engine-size=1.6&transmission=Manual&quantity-of-doors=3&seller-type=private&keywords=cooper&page="
-row_headers = ['ID','Age','Price','Miles']
+row_headers = ['MatchID','HomeTeam','AwayTeam','Scorer', 'ScoredFor','Time']
 
-with open('cars.csv', 'w', newline='') as myfile:
+with open('results.csv', 'w', newline='') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     wr.writerow(row_headers)
 print (row_headers)
 
-for i in range(1, 41):
-    url = url_front + str(i)
-    #print(url)
+for i in range(3, 4):
+    url = 'https://www.premierleague.com/match/'
+    print(url)
     #print ("Page " + str(i))
-    download_details(url)
+    download_details(url,i)
